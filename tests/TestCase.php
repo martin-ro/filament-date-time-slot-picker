771b3dc8136c14af182af48d04505afd4dbfb96a
Zep Fietje <hey@zepfietje.com>
2022-10-16 12:35:57 +0200

    Clean up TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace ZepFietje\FilamentDateTimeSlotPicker\Tests;
 
 use Filament\FilamentServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use ZepFietje\FilamentDateTimeSlotPicker\FilamentDateTimeSlotPickerServiceProvider;
@@ -13,10 +12,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'ZepFietje\\FilamentDateTimeSlotPicker\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -26,15 +21,5 @@ class TestCase extends Orchestra
             FilamentServiceProvider::class,
             FilamentDateTimeSlotPickerServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-date-time-slot-picker_table.php.stub';
-        $migration->up();
-        */
     }
 }
