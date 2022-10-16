cffcafee7fdd41a684d97580a1d72052fd930b6d
Zep Fietje <hey@zepfietje.com>
2022-10-16 12:39:41 +0200

    Clean up service provider

--- src/FilamentDateTimeSlotPickerServiceProvider.php
+++ src/FilamentDateTimeSlotPickerServiceProvider.php
@@ -9,18 +9,6 @@ class FilamentDateTimeSlotPickerServiceProvider extends PluginServiceProvider
 {
     public static string $name = 'filament-date-time-slot-picker';
 
-    protected array $resources = [
-        // CustomResource::class,
-    ];
-
-    protected array $pages = [
-        // CustomPage::class,
-    ];
-
-    protected array $widgets = [
-        // CustomWidget::class,
-    ];
-
     protected array $styles = [
         'plugin-filament-date-time-slot-picker' => __DIR__.'/../resources/dist/filament-date-time-slot-picker.css',
     ];
@@ -29,12 +17,10 @@ class FilamentDateTimeSlotPickerServiceProvider extends PluginServiceProvider
         'plugin-filament-date-time-slot-picker' => __DIR__.'/../resources/dist/filament-date-time-slot-picker.js',
     ];
 
-    // protected array $beforeCoreScripts = [
-    //     'plugin-filament-date-time-slot-picker' => __DIR__ . '/../resources/dist/filament-date-time-slot-picker.js',
-    // ];
-
     public function configurePackage(Package $package): void
     {
-        $package->name(static::$name);
+        $package
+            ->name(static::$name)
+            ->hasViews();
     }
 }
