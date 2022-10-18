0f311b61a6c93620a72f43569f600aa373a8a3b3
Zep Fietje <hey@zepfietje.com>
2022-10-18 15:13:14 +0200

    Dehydrate state to array Carbon instances

--- src/DateTimeSlotPicker.php
+++ src/DateTimeSlotPicker.php
@@ -4,12 +4,26 @@ namespace ZepFietje\FilamentDateTimeSlotPicker;
 
 use Closure;
 use Filament\Forms\Components\Field;
+use Illuminate\Support\Carbon;
 
 class DateTimeSlotPicker extends Field
 {
     protected string $view = 'filament-date-time-slot-picker::field';
 
     protected array|Closure $options = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dehydrateStateUsing(static function (?array $state) {
+            if (blank($state)) {
+                return null;
+            }
+
+            return array_map(fn ($date): Carbon => Carbon::parse($date), $state);
+        });
+    }
 
     public function options(array|Closure $options): static
     {
